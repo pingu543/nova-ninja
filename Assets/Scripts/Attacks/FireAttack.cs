3d5b0279e3814649200d0abe693fd017ade17d60
AmppSSBM <alek423s@gmail.com>
2025-10-23 21:22:03 -0400

    Added collision for attack (and defense)
    Complete logic for all 3 attack spheres. Includes logic for Projectile vs Projectile/Wall/Player/etc

--- Assets/Scripts/Attacks/FireAttack.cs
+++ Assets/Scripts/Attacks/FireAttack.cs
@@ -2,25 +2,74 @@ using UnityEngine;
 
 public class FireAttack : MonoBehaviour
 {
-    //[SerializeField] private float projectileSpeed = 10.0f;
-    [SerializeField] private float projectileLife = 5.0f;
+    private const string FIRE_TAG = "Fire";
+    private const string WATER_TAG = "Water";
+    private const string EARTH_TAG = "Earth";
+    private const string PLAYER_TAG = "Player";
+    private const string TROPHY_TAG = "Trophy";
 
+    //[SerializeField] private float projectileSpeed = 10.0f;
+    [SerializeField] public float defaultLifespan = 8f;
+    [SerializeField] public float bounceLifespan = 3f;
+
+    private Vector3 preCollisionVelocity;
+    private Rigidbody rb;
 
     Renderer renderer;
-    //Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         renderer = GetComponent<Renderer>();
         renderer.material.color = Color.firebrick;
-        // rb = GetComponent<Rigidbody>(); //don't need because movement has moved to PlayerAttack
-        Destroy(gameObject, projectileLife);
+
+        rb = GetComponent<Rigidbody>();
+
+        Destroy(gameObject, defaultLifespan);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision) // Called when this collider/rigidbody has begun touching another rigidbody/collider
     {
+        GameObject otherObject = collision.gameObject;
+        string otherTag = otherObject.tag;
 
+        // loss
+        if (otherTag == WATER_TAG)
+        {
+            Destroy(gameObject);
+        }
+        // win
+        else if (otherTag == EARTH_TAG)
+        {
+            // if earth wall
+            WallMarker wallMarker = otherObject.GetComponent<WallMarker>();
+            if (wallMarker != null)
+            {
+                Destroy(otherObject);
+                if (rb != null)
+                {
+                    rb.linearVelocity = preCollisionVelocity;
+                }
+            }
+            else // if earth projectile
+            {
+                Destroy(gameObject, bounceLifespan);
+            }
+        }
+        // player
+        else if (otherTag == PLAYER_TAG)
+        {
+            Destroy(gameObject);
+            // DAMAGE (or lessen rank. But it might exist elsewhere)
+        }
+        // target?
+        //
+        // wall?
+        //
+        // tie (and environment)
+        else
+        {
+            Destroy(gameObject, bounceLifespan);
+        }
     }
 }
---
+++ Assets/Scripts/Attacks/WallMarker.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+// This component is purely a marker to identify any object as a "Wall". It is lazy, but the apple doesn't fall far from the tree.
+public class WallMarker : MonoBehaviour{}
